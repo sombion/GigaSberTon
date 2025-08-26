1336d1338ba222298e990bcedbb9bcae8daf8334
sombion <sombion@mail.ru>
2025-08-26 22:29:34 +0300

    Connect db

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-from app.config import settings
+from backend.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
